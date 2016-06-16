69c4363cf8526c40c1687529afb794a12582f704
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2016-06-15 23:24:21 -0400

    Updated get_config_paths() to support pkg_resources
    When the package is installed in development mode, use pkg_resources to
    find the config files.

--- src/cmdline/config.py
+++ src/cmdline/config.py
@@ -1,11 +1,22 @@
 import os
 import sys
 
+try:
+    import pkg_resources
+
+    d = pkg_resources.get_distribution('metermaid')
+    pkg_locations = (
+        os.path.join(d.location, 'config'),
+        os.path.join(os.path.dirname(d.location), 'config'),
+    )
+except ImportError:
+    pkg_locations = ()
+
 
 def get_config_paths(filename=None):
     script_name = os.path.basename(sys.argv[0])
 
-    for dirpath in (
+    for dirpath in pkg_locations + (
             os.path.join(sys.prefix, 'config'),
             '/etc/{}'.format(script_name),
             os.path.expanduser('~/.{}'.format(script_name)),
