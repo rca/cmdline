1c1882f6471af7e734b2e66e59c2b40f993b3844
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2016-06-27 12:25:42 -0400

    Updated tests

--- src/tests/test_settings.py
+++ src/tests/test_settings.py
@@ -26,6 +26,18 @@ CONFIG_YAML = REMOTE_ADDR
 CONFIG_YAML2 = REMOTE_ADDR + REMOTE_PORT
 CONFIG_YAML3 = REMOTE_PORT.replace('1234', '5678')
 
+CONFIG_YAML_REQUIRED = """
+FOO:
+  _positional: true
+  help: the foo
+"""
+
+CONFIG_YAML_COMMANDS = """
+_COMMANDS:
+  foo:
+    help: the foo
+"""
+
 
 assert_equal(None, settings._compiled_settings)
 
@@ -100,3 +112,42 @@ class SettingsTestCase(unittest.TestCase):
         SettingsParser.compile_settings(sys_argv=[])
 
         self.assertEqual(1, len(self.settings.run.mock_calls))
+
+    @mock.patch('argparse.ArgumentParser.exit')
+    def test_required(self, *mocks):
+        """
+        Test a required argument
+        """
+        mocked = mock.Mock()
+        mocked.side_effect = [yaml.load(StringIO(CONFIG_YAML_REQUIRED)), None, None]
+
+        self.settings.get_settings_from_file = mocked
+
+        settings_run = self.settings.run
+
+        self.settings.run = mock.Mock(side_effect=lambda: settings_run())
+
+        SettingsParser.compile_settings(sys_argv=[])
+
+        exit_mock = mocks[0]
+        error_message = exit_mock.mock_calls[0][1][1]
+        expected = 'the following arguments are required: foo'
+        self.assertEqual(True, expected in error_message, '"{}", not in "{}"'.format(expected, error_message))
+
+        self.assertEqual(1, len(self.settings.run.mock_calls))
+
+    def test_subcommand_only_in_commands_entry(self, *mocks):
+        mocked = mock.Mock()
+        mocked.side_effect = [yaml.load(StringIO(CONFIG_YAML_COMMANDS)), None, None]
+
+        self.settings.get_settings_from_file = mocked
+
+        # process settings files
+        self.settings.run()
+
+        # create a parser
+        settings_parser = SettingsParser()
+        settings_parser.compile_settings(sys_argv=[])
+
+        # ensure the subcommand was created
+        self.assertEqual(['foo'], list(settings_parser.subparsers.choices.keys()))
