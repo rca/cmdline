3acb182170dca727e650f9012b9990567720e1d0
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2016-06-28 09:43:36 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if not SCRIPT_DIR:
 
 
 setup(name='cmdline',
-      version='0.1.1',
+      version='0.1.2',
       description='Utilities for consistent command line tools',
       author='Roberto Aguilar',
       author_email='r@rreboto.com',
